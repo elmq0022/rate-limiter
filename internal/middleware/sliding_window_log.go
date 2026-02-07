ac04ad3771803456a75f51dd4f490389c38c9922
Aaron Elmquist <aaron.elmquist@protonmail.com>
2026-02-06 21:56:48 -0600

    adds the sliding window log algo

--- internal/middleware/sliding_window_log.go
+++ internal/middleware/sliding_window_log.go
@@ -1,7 +1,59 @@
 package middleware
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type SlidingWindowLog struct {
+	maxRequests int
+	requestLog  []time.Time
+	logDuration time.Duration
+	mu          sync.Mutex
+}
+
+func (s *SlidingWindowLog) Allow() bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	now := time.Now()
+	start := now.Add(-s.logDuration)
+
+	index := 0
+	for i, t := range s.requestLog {
+		if t.Before(start) {
+			index = i + 1
+		} else {
+			break
+		}
+	}
+
+	n := copy(s.requestLog, s.requestLog[index:])
+	s.requestLog = s.requestLog[:n]
+
+	if n < s.maxRequests {
+		s.requestLog = append(s.requestLog, now)
+		return true
+	}
+	return false
+}
+
+var swl = SlidingWindowLog{
+	maxRequests: 100,
+	requestLog:  []time.Time{},
+	logDuration: 100 * time.Second,
+}
 
 func SlidingWindowLogMW(h http.HandlerFunc) http.HandlerFunc {
-	return h
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !swl.Allow() {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusTooManyRequests)
+			json.NewEncoder(w).Encode(map[string]string{"msg": "Too Many Requests"})
+			return
+		}
+		h(w, r)
+	}
 }
